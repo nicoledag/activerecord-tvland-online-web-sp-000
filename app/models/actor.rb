e80f65cdfb6a49f858a5d5e923f5a492aeab587e
Nicole D <nnardella@gmail.com>
2019-01-14 17:15:53 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
-  
+
+  has_many :characters      #add actor_id to character table
+  has_many :shows,through: :characters
+
+
+  def full_name
+    "#{self.first_name} " + "#{self.last_name}"
+  end
+
+  def list_roles
+    binding.pry
+    self.characters.collect do |char|
+      "#{char.name} - #{char.show.name}"
+    end
+  end
+
+
 end
