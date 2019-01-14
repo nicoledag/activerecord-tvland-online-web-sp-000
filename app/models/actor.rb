8b3184b17c97da489e781031b950ae80a03e3c08
Nicole D <nnardella@gmail.com>
2019-01-14 17:41:31 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+
+  has_many :characters      #add actor_id to character table
+  has_many :shows,through: :characters
+
+
+  def full_name
+    "#{self.first_name} " + "#{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.collect do |char|
+      "#{char.name} - #{char.show.name}"
+    end
+  end
+
+
 end
