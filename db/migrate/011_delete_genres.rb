f53923de4b59b8a9ad3c721e98f3586a521b17b5
Nicole D <nnardella@gmail.com>
2019-01-14 17:32:42 +0000

    Automatically backed up by Learn

--- db/migrate/011_delete_genres.rb
+++ db/migrate/011_delete_genres.rb
@@ -1,8 +1,5 @@
-class CreateGenres < ActiveRecord::Migration[5.1]
+class DeleteGenres < ActiveRecord::Migration[5.1]
   def change
-    delete_table :genres do |t|
-      #primary key of :id is created for us!
-      t.string :name
-    end
+    drop_table :genres
   end
 end
